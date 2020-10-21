e5c7a3086e0c7063192ca9a6c858e8edb11c395f
Sandy Sayuri Katsumata Ishii <66523335+Sandy-Sayuri@users.noreply.github.com>
2020-10-21 10:33:54 -0300

    aps completa
    biblioteca

---
+++ src/model/AuthorBD.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author sandy
+ */
+public class AuthorBD {
+    private String nome;
+    private String fname;
+    private int author_id ;
+    public AuthorBD(String nome, String fname) {
+        this.nome = nome;
+        this.fname = fname;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public int getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(int author_id) {
+        this.author_id = author_id;
+    }
+
+  
+}
---
+++ src/model/BookAuthors.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author sandy
+ */
+public class BookAuthors implements Serializable {
+   @Id
+   @Column(unique=true)
+   @GeneratedValue (strategy=GenerationType.IDENTITY)
+   private int BookAuthors_id ;
+   @ManyToOne
+   @JoinColumn(name="author_id")
+   private int author_id ;
+   @ManyToOne
+   @JoinColumn(name="isbn")
+   private String isbn;
+
+    public BookAuthors() {
+    }
+
+    public int getBookAuthors_id() {
+        return BookAuthors_id;
+    }
+
+    public void setBookAuthors_id(int BookAuthors_id) {
+        this.BookAuthors_id = BookAuthors_id;
+    }
+
+    public int getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(int author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+   
+}
